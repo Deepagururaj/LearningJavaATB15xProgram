a30e525c320e0cf100519804c96665a6f752155e
DeepaRagunath24 <deepa.ragunath@streamoid.com>
2025-12-18 22:55:52 +0530

    commit codes

---
+++ src/ex_02_Java_Basics/Lab023_Byte.java
@@ -0,0 +1,14 @@
+package ex_02_Java_Basics;
+
+public class Lab023_Byte {
+    public static void main(String[] args) {
+        long age = 10L;
+        char c = 10;
+        System.out.println(c);
+        System.out.println(age);
+
+        String s = "Deepa"; // Non Primitive.
+        // Deepa -> D,e,e,p,a - chars-String is bunch of chars.
+
+    }
+}
