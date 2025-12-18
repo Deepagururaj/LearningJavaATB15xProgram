7771d292936d1fd335fa7fcabd4ffd9359ee7ecb
DeepaRagunath24 <deepa.ragunath@streamoid.com>
2025-12-18 22:39:57 +0530

    commit my codes

---
+++ src/ex_02_Java_Basics/Lab015_Variables.java
@@ -0,0 +1,21 @@
+package ex_02_Java_Basics;
+
+public class Lab015_Variables {
+    public static void main(String[] args) {
+
+        // ```
+        // data_type variable_name =  variable_value
+        //```
+
+        // Variable - Local Variable
+        byte age_deepa = 35;
+        // byte -> data type - type of container
+        //  age_sunil ->  variable_name |  identifier
+        // = -> Assignment Operator ?  (after some time)
+        // 45 -> Variable Value |  Literal
+        age_deepa = 112;
+        age_deepa = 119;
+        System.out.println(age_deepa);
+
+    }
+}
