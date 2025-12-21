79a56487759ca8eb1c084529ad8e2460b0237b27
DeepaRagunath24 <deepa.ragunath@streamoid.com>
2025-12-22 01:09:06 +0530

    push ex_2 codes

---
+++ src/ex_02_Java_Basics/Lab028_Final_Example.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+
+public class Lab028_Final_Example {
+    public static void main(String[] args) {
+        int _ = 10;
+        int $ = 10;
+        // int @ = 10;
+        // System.out.println(_);
+        System.out.println($);
+        // Java will not show error for Symbols like _,$ but it will not provide any output in latest JDK.
+    }
+}
