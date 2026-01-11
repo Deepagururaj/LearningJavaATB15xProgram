3a43ca660b85b6710072843e3d52a7f6e6cae040
DeepaRagunath24 <deepa.ragunath@streamoid.com>
2026-01-11 16:27:01 +0530

    push operators

---
+++ src/ex_04_Operators/Lab042_Interview_Contact_Plus.java
@@ -0,0 +1,22 @@
+package ex_04_Operators;
+
+public class Lab042_Interview_Contact_Plus {
+    public static void main(String[] args) {
+        // +
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+
+        // + -> String -> Concatenation
+        String first_name = "Deepa";
+        String last_name = "Gururaj";
+        System.out.println(first_name+last_name); // concatenation
+
+//        System.out.println(first_name-last_name);
+//        System.out.println(first_name*last_name);
+//        System.out.println(first_name/last_name);
+
+        // + - behave arch int
+        // + - concat String
+    }
+}
